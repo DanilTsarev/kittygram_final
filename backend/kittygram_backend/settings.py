54c5fadfe30d16bd08942d42048eb2786654a483
Данил Царев <Drsif@yandex.ru>
2023-08-12 00:13:18 +0300

    CI/CD for Kittygram

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,11 @@
 # flake8: noqa
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-env_path = Path(__file__).resolve().parent.parent / '.env'
-load_dotenv(dotenv_path=env_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
